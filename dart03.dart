7526f47c3875868b3debae9f77ca3a15d624ee36
ozgurustun <ozgurustundev@gmail.com>
2020-10-07 00:42:26 +0300

    Access modifiers and optional field on constructor

---
+++ dart03.dart
@@ -0,0 +1,15 @@
+//Functions are also objects and have types
+
+//in dart, there is no access modifiers, _ is used to make private
+
+List<String> _strList = new List();
+
+//Optinal fields will be declared within curly braces {}
+
+class OptClass {
+
+  int number1;
+
+  OptClass({this.number1});
+
+}
